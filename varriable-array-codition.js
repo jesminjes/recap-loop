b76153b3e280eb936b1fe90ee6864e02dba80622
jesminjes <mstjesmina56@gmail.com>
2022-04-07 23:43:54 +0600

    first commit

---
+++ varriable-array-codition.js
@@ -0,0 +1,21 @@
+var bottleColor = 'yellow';
+var waterQuantity = 1;
+var isFull = false;
+
+// array 
+var items = ['bottle', 'mug', 'papper', 'pen'];
+items.indexOf('logens');  // -1
+items.push('envelope');
+items.concat('watch');
+items.pop();
+
+//conditional 
+if(items.length >=4){
+    console.log('You have too my stuff on your desk.')
+}
+else if(items.length == 4){
+    console.log('you only have one hali items')
+}
+else{
+    console.log('wow! You have a clean desk.')
+}
